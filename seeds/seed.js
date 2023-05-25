c989ac27e1441279552694b61f3b3203308296c6
Tifni Shroll <tasshroll@gmail.com>
2023-05-24 21:02:58 -0700

    seed the blog and user tables, app listening

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,1 +1,30 @@
-gi
+const sequelize = require('../config/connection');
+const { User, Blog, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData);
+  // const users = await User.bulkCreate(userData, {
+  //   individualHooks: true,
+  //   returning: true,
+  // });
+
+
+  const blog = await Blog.bulkCreate(blogData);
+
+
+  //   for (const project of projectData) {
+  //     await Project.create({
+  //       ...project,
+  //       user_id: users[Math.floor(Math.random() * users.length)].id,
+  //     });
+  //   }
+
+  process.exit(0);
+};
+
+seedDatabase();
